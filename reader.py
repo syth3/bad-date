333fd5187ea5df716fb085b8ee573d3ae56e03cd
ew0k <jakebrown188@gmail.com>
2018-11-15 20:43:59 -0500

    Updated MacEntry object and way data is stored in the dictionary

--- reader.py
+++ reader.py
@@ -5,40 +5,49 @@ import sys
 from datetime import datetime
 
 class MacEntry:
-    def __init__(self, date, type):
-        self.date = date
-        self.type = type
-
+    def __init__(self, access_time, modify_time, change_time):
+        self.access_time = access_time
+        self.modify_time = modify_time
+        self.change_time = change_time
 
 file_name = os.path.abspath(sys.argv[1])
 
 with open(file_name) as csv_file:
     csv_reader = csv.DictReader(csv_file)
-    # csv.reader(csv_file, delimiter=',')
     line_count = 0
     file_name_dict = {}
     for key in csv_reader:
+        date_type = key["Type"]
+        access_time = None
+        modify_time = None
+        change_time = None
         file_name = key["File Name"]
         date = datetime.strptime(key["Date"], '%a %m %d %Y %H:%M:%S')
-        mac_object = MacEntry(date, key["Type"])
+        if 'a' in date_type:
+            access_time = datetime.strptime(key["Date"], '%a %m %d %Y %H:%M:%S')
+        if 'm' in date_type:
+            modify_time = datetime.strptime(key["Date"], '%a %m %d %Y %H:%M:%S')
+        if 'c' in date_type:
+            change_time = datetime.strptime(key["Date"], '%a %m %d %Y %H:%M:%S')
+
         if file_name in file_name_dict:
-            file_name_dict[file_name].append(mac_object)
+            if access_time != None:
+                file_name_dict[file_name].access_time = access_time
+            if modify_time != None:
+                file_name_dict[file_name].modify_time = modify_time
+            if change_time != None:
+                file_name_dict[file_name].change_time = change_time
         else:
-            file_name_dict[file_name] = [mac_object]
+            file_name_dict[file_name] = MacEntry(access_time, modify_time, change_time)
+
     
 for key in file_name_dict:
     count = 0
-    print("key =", key)
+    print("File =", key)
     print("==========================================")
-    obj_list = file_name_dict[key]
-    for obj in obj_list:
-        count += 1
-        print("Date:", obj.date)
-        print("Type:", obj.type)
-        if count == len(obj_list):
-            pass
-        else:
-            print()
+    print("access_time =", file_name_dict[key].access_time)
+    print("modify_time =", file_name_dict[key].modify_time)
+    print("change_time =", file_name_dict[key].change_time)
     print("==========================================")
     print()
   
